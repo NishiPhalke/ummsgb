b978b7bb39f5e85e3a23737667788509fcf8a47a
hpratt <hpratt111@live.com>
2020-09-09 17:32:49 -0400

    fix stacked track bug, clean up some warnings

--- src/components/search/refseqsearchbox.tsx
+++ src/components/search/refseqsearchbox.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useCallback } from 'react';
 import { Form, Search } from 'semantic-ui-react';
 import { REFSEQ_AUTOCOMPLETE_QUERY } from './queries';
-import { uniq, isCoordinate, refsequniq } from './utils';
+import { isCoordinate, refsequniq } from './utils';
 import { RefSeqSearchBoxProps, Result } from './types';
 
 const RefSeqSearchBox: React.FC<RefSeqSearchBoxProps> = (props) => {
     const [selectedGene, setSelectedGene] = useState<Result | undefined>();
     const [results, setResults] = useState<Result[]>();
-    const [gene, setGene] = useState<string>();
 
     const onSubmit = useCallback(() => {
         if (selectedGene && isCoordinate(selectedGene.description))
@@ -17,7 +16,7 @@ const RefSeqSearchBox: React.FC<RefSeqSearchBoxProps> = (props) => {
         let g = (selectedGene && selectedGene.description) ? selectedGene.description :  results && results[0].description;
         if (g === undefined) return;      
         props.onSearchSubmit && props.onSearchSubmit(g);
-    }, [gene, results, props]);
+    }, [results, props, selectedGene]);
     const onSearchChange = useCallback(
         async (e, { value }) => {
             const response = await fetch('http://35.201.115.1/graphql', {
@@ -43,9 +42,7 @@ const RefSeqSearchBox: React.FC<RefSeqSearchBoxProps> = (props) => {
                 '-' +
                 result.coordinates.end,
             }));
-            if(sresults.length) { 
-                setGene(value)
-            } else {
+            if(!sresults.length) {
                 setSelectedGene({ description: value })
             }            
             setResults(sresults.length ? sresults : [{ title: value, description: "" }])
